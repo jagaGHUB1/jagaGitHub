6c1bce38e60ed89ba750e312200f2aefd8de3cef
cubic <cubic@192.168.0.34>
2018-06-21 17:09:02 +0530

    hiii-praveen

---
+++ tstring/src/tst/student.java
@@ -0,0 +1,23 @@
+package tst;
+
+public class student {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		
+		int b=20;
+		
+		int c;
+		
+		c=a+b;
+		
+		
+		
+		System.out.println("A and B Values :"+c);
+		
+		
+		
+	}
+	
+}
